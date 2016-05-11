939c45149ea1400008668625bcce08eb97175cee
umutmobven <umut@mobven.com>
2016-05-11 19:01:35 +0300

    Add files via upload

---
+++ IOS/Samples-Project/Swift/MobvenBugKitTestSwiftApplication/MobvenBugKitTestSwiftApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MobvenBugKitTestSwiftApplication
+//
+//  Created by Baran Kaan Sert on 11.03.2016.
+//  Copyright © 2016 Baran Kaan Sert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.yellowColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
